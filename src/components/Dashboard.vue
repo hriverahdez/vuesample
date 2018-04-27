bf4a257b00744c1ccb956d2e34f9b3e03182b8a8
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-04-27 10:07:18 +0200

    Instalación axios, i18n y primera llamada axios

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  h1 Dashboard
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created () {
+    axios.get('http://stage.do.linkitox.com/i18n-messages/es~en')
+        .then(response => {
+          console.log(response)
+        })
+        .catch(e => {
+          console.log(e)
+        })
+  }
+}
+</script>
