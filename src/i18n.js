0ace0b15a3fd00c1394f489f175b92d45c13004e
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-04-30 08:15:36 +0200

    i18n funcionando con archivos locales

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import {store} from '@/store'
+// import {store} from '@/store'
 
 Vue.use(VueI18n)
 
-let p = 'pepe'
+// let p = 'pepe'
 
-setTimeout(() => {
-  p = store.state.words
-  console.log(p)
-}, 1000)
+// setTimeout(() => {
+//   p = store.state.words
+//   console.log(p)
+// }, 1000)
 
 export const i18n = new VueI18n({
   locale: 'en', // set locale
-  messages: p
+  messages: {
+    'en': require('./locales/en.json'),
+    'es': require('./locales/es.json')
+  }
 })
 
-
-
-console.log('entra', store.state.pepe.hola)
-console.log(i18n)
+// console.log('entra', store.state.pepe.hola)
+// console.log(i18n)
