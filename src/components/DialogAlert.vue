9dd7030f867cde2a4c9f794a4a4bf90b15fb2974
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-05-11 14:25:56 +0200

    Modificacion componente 'dialog-alert'

--- src/components/DialogAlert.vue
+++ src/components/DialogAlert.vue
@@ -1,19 +1,31 @@
 
 <template lang="pug">
 
-    div.alert__container
-        v-alert(
-            :value="$store.state.alert.show"
-            :type="$store.state.alert.type"
-            transition="fade-transition"
-        ) {{ $store.state.alert.message }}
+    v-dialog(v-model="$store.state.alert.show" max-width="500px")
+        v-card
+            v-alert(
+                :value="$store.state.alert.show"
+                :type="$store.state.alert.type"
+                transition="fade-transition"
+            ) {{ $store.state.alert.message }}
+            v-card-actions
+                v-spacer
+                v-btn(
+                color="form_elements_color"
+                flat
+                @click.native="closeDialog"
+                ) {{ $store.state.alert.buttonText }}
 
 </template>
 
 <script>
 // Show message alerts in dialogs
 export default {
-  name: 'dialog-alert'
+  name: 'dialog-alert',
+  methods: {
+    closeDialog () {
+      this.$store.commit('closeAlertDialog')
+    }
+  }
 }
 </script>
-
