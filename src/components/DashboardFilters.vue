3cfe7aa9db89a7abe30acd60b60bf6137679e7de
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-05-14 13:33:13 +0200

    Añadidos tres filtros al dashboard

--- src/components/DashboardFilters.vue
+++ src/components/DashboardFilters.vue
@@ -8,6 +8,8 @@
                 item-text="name"
                 :label="this.$t('dashboard_view.apps')"
                 tags
+                autocomplete
+                clearable
             )
                 template(slot="selection" slot-scope="data")
                     v-chip(
@@ -17,13 +19,44 @@
                     )
                         v-avatar(class="accent") {{ data.item.slice(0, 1).toUpperCase() }}
                         | {{ data.item }}
-
+        v-flex(xs4)
+            v-select(
+                v-model="emptyNetworks"
+                :items="networks"
+                :label="this.$t('dashboard_view.networks')"
+                tags
+                autocomplete
+                clearable
+            )
+                template(slot="selection" slot-scope="data")
+                    v-chip(
+                        :selected="data.selected"
+                        close
+                        @input="data.parent.selectItem(data.item)"
+                    ) {{ data.item }}
+        v-flex(xs4)
+            v-select(
+                v-model="emptyFormats"
+                :items="formats"
+                :label="this.$t('dashboard_view.formats')"
+                tags
+                autocomplete
+                clearable
+            )
+                template(slot="selection" slot-scope="data")
+                    v-chip(
+                        :selected="data.selected"
+                        close
+                        @input="data.parent.selectItem(data.item)"
+                    ) {{ data.item }}
 
 </template>
 
 <script>
 export default {
+
   name: 'dashboard-filters',
+
   data () {
     return {
       apps: [
@@ -32,7 +65,22 @@ export default {
         'Ejemplo app 1',
         'Ejemplo app 2'
       ],
-      emptyApps: []
+      formats: [
+        'Banner',
+        'Banner premium',
+        'Interstitials',
+        'Interstitiasl premium',
+        ' Reward video'
+      ],
+      networks: [
+        'Addcolony',
+        'Addmob',
+        'Applovin',
+        'Chartboost'
+      ],
+      emptyApps: [],
+      emptyFormats: [],
+      emptyNetworks: []
     }
   }
 }
