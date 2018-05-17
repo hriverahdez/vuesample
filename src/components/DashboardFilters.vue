1312f35d3d75a1e789a5b4a5e9a5312b00b1f0a8
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-05-17 11:05:35 +0200

    Modificación dashboard filters

--- src/components/DashboardFilters.vue
+++ src/components/DashboardFilters.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   v-container(grid-list-md text-xs-center)
     v-layout(row wrap)
-        v-flex(xs4)
+        v-flex(xs3)
             v-select(
-                v-model="emptyApps"
-                :items="apps"
+                v-model="emptyApp"
+                :items="app"
                 item-text="name"
-                :label="this.$t('dashboard_view.apps')"
+                :label="this.$t('dashboard_view.app')"
                 tags
                 autocomplete
                 clearable
@@ -19,11 +19,11 @@
                     )
                         v-avatar(class="accent") {{ data.item.slice(0, 1).toUpperCase() }}
                         | {{ data.item }}
-        v-flex(xs4)
+        v-flex(xs3)
             v-select(
-                v-model="emptyNetworks"
-                :items="networks"
-                :label="this.$t('dashboard_view.networks')"
+                v-model="emptyCountry"
+                :items="country"
+                :label="this.$t('dashboard_view.country')"
                 tags
                 autocomplete
                 clearable
@@ -34,11 +34,28 @@
                         close
                         @input="data.parent.selectItem(data.item)"
                     ) {{ data.item }}
-        v-flex(xs4)
+
+        v-flex(xs3)
             v-select(
-                v-model="emptyFormats"
-                :items="formats"
-                :label="this.$t('dashboard_view.formats')"
+                v-model="emptyFormat"
+                :items="format"
+                :label="this.$t('dashboard_view.format')"
+                tags
+                autocomplete
+                clearable
+            )
+                template(slot="selection" slot-scope="data")
+                    v-chip(
+                        :selected="data.selected"
+                        close
+                        @input="data.parent.selectItem(data.item)"
+                    ) {{ data.item }}
+
+        v-flex(xs3)
+            v-select(
+                v-model="emptyNetwork"
+                :items="network"
+                :label="this.$t('dashboard_view.network')"
                 tags
                 autocomplete
                 clearable
@@ -59,28 +76,34 @@ export default {
 
   data () {
     return {
-      apps: [
+      app: [
         'Android',
         'IOS',
         'Ejemplo app 1',
         'Ejemplo app 2'
       ],
-      formats: [
+      country: [
+        'Spain',
+        'England',
+        'Anngola'
+      ],
+      format: [
         'Banner',
         'Banner premium',
         'Interstitials',
         'Interstitiasl premium',
         ' Reward video'
       ],
-      networks: [
+      network: [
         'Addcolony',
         'Addmob',
         'Applovin',
         'Chartboost'
       ],
-      emptyApps: [],
-      emptyFormats: [],
-      emptyNetworks: []
+      emptyApp: [],
+      emptyCountry: [],
+      emptyFormat: [],
+      emptyNetwork: []
     }
   }
 }
