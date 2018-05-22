5c25bd8da259c0bdb500ec0fa0b8132f5b480bf8
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-05-22 11:47:33 +0200

    Cambios query para aceptar parámetros

--- src/graphql/report.js
+++ src/graphql/report.js
@@ -1,16 +1,14 @@
 import gql from 'graphql-tag'
 
 export const GET_DASHBOARD_REPORT_DATA = gql`
-{
- stats(groupBy: [DATE]) {
-    label
-    requests
-    imps
-    clicks
-    fillRate
-    ctr
-    revenue
-    ecpm
- }
+query ($groupBy: [StatsGroupBy], $filter: [StatsFilter]) {
+    stats (groupBy: $groupBy, filter: $filter) {
+        label
+        requests
+        imps
+        clicks
+        revenue
+        ecpm
+    }
 }
 `
