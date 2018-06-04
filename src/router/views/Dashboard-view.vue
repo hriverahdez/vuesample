a96499fe98d0d3e819cf5e49051fa8b6a0806e73
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-06-04 12:40:25 +0200

    Creación de cuanta funcionando desde el Dashboard

--- src/router/views/Dashboard-view.vue
+++ src/router/views/Dashboard-view.vue
@@ -12,7 +12,7 @@
 
 
 <script>
-  import { mapActions, mapGetters } from 'vuex'
+  import { mapActions, mapGetters, mapMutations } from 'vuex'
   // Components imports
   import DashboardDataTable from '@/components/dashboard/DashboardDataTable'
   import DashboardTabs from '@/components/dashboard/DashboardTabs'
@@ -20,6 +20,7 @@
   import { APP_DATA } from '@/graphql/app'
   import { GET_DASHBOARD_REPORT_DATA } from '@/graphql/report'
   import { GET_DATA_FILTERS } from '@/graphql/config'
+  import { CREATE_NEW_ACCOUNT, GET_ACCOUNTS } from '@/graphql/account'
 
   export default {
     name: 'dashboard-view',
@@ -86,10 +87,65 @@
     },
     methods: {
       ...mapActions([
+        'accountDialogStatusAction',
+        'accountSchemaAction',
         'appDataAction',
         'dashboardFiltersAction',
         'statsDataAction'
-      ])
+      ]),
+      ...mapMutations(['SET_ALERT_MESSAGE']),
+      createAccount (name, description, disabled) {
+        this.$apollo.mutate({
+          mutation: CREATE_NEW_ACCOUNT,
+          context: {
+            uri: 'account'
+          },
+          variables: {
+            input: {
+              name: name,
+              description: description,
+              disabled: disabled || false
+            }
+          },
+          update: (store, { data: { createAccount } }) => {
+            // Read the data from our cache for this query.
+            const data = store.readQuery({ query: GET_ACCOUNTS })
+            // Add our tag from the mutation to the end
+            data.accounts.push(createAccount)
+            // Write our data back to the cache.
+            store.writeQuery({ query: GET_ACCOUNTS, data })
+          }
+        })
+        .then(() => {
+          this.SET_ALERT_MESSAGE({
+            show: true,
+            type: 'success',
+            message: this.$t('accounts_view.new_success'),
+            buttonText: this.$t('buttons.close')
+          })
+        })
+        this.accountDialogStatusAction(false)
+        this.accountSchemaAction({
+          name: '',
+          description: '',
+          disabled: ''
+        })
+      }
+    },
+    mounted () {
+      // Receive events from components
+      this.$root.$on('createAccount', (name, description, disabled) => {
+        this.createAccount(name, description, disabled)
+      })
+      this.$root.$on('deleteAccount', (account) => {
+        this.deleteAccount(account)
+      })
+      this.$root.$on('editAccount', (id, name, description, disabled) => {
+        this.editAccount(id, name, description, disabled)
+      })
+    },
+    beforeDestroy () {
+      this.$root.$off()
     }
   }
 </script>
