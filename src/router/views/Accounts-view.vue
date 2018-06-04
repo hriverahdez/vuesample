039bd0bc9b9cc5624b5c5caeecb599e700f0aa0f
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-06-04 13:29:19 +0200

    Creación de mixin de cuentas

--- src/router/views/Accounts-view.vue
+++ src/router/views/Accounts-view.vue
@@ -8,9 +8,8 @@
 </template>
 
 <script>
-  import { mapActions, mapMutations } from 'vuex'
-  // Queries
-  import { CREATE_NEW_ACCOUNT, DELETE_ACCOUNT, GET_ACCOUNTS, UPDATE_ACCOUNT } from '@/graphql/account'
+  // Mixins
+  import accountMixin from '@/mixins/accountMixin'
   // Components
   import AccountsDataTable from '@/components/accounts/AccountsDataTable'
 
@@ -18,140 +17,6 @@
     components: {
       AccountsDataTable
     },
-    apollo: {
-      accounts: {
-        query: GET_ACCOUNTS,
-        context: {
-          uri: 'account'
-        },
-        loadingKey: 'loading',
-        update (data) {
-          this.accountsDataAction(data.accounts)
-        }
-      }
-    },
-    methods: {
-      ...mapActions([
-        'accountsDataAction',
-        'accountDialogStatusAction',
-        'accountSchemaAction',
-        'editedIndexStatusAction'
-      ]),
-      ...mapMutations(['SET_ALERT_MESSAGE']),
-      // Create account
-      createAccount (name, description, disabled) {
-        this.$apollo.mutate({
-          mutation: CREATE_NEW_ACCOUNT,
-          context: {
-            uri: 'account'
-          },
-          variables: {
-            input: {
-              name: name,
-              description: description,
-              disabled: disabled || false
-            }
-          },
-          update: (store, { data: { createAccount } }) => {
-            // Read the data from our cache for this query.
-            const data = store.readQuery({ query: GET_ACCOUNTS })
-            // Add our tag from the mutation to the end
-            data.accounts.push(createAccount)
-            // Write our data back to the cache.
-            store.writeQuery({ query: GET_ACCOUNTS, data })
-          }
-        })
-        .then(() => {
-          this.$apollo.queries.accounts.refresh()
-          this.SET_ALERT_MESSAGE({
-            show: true,
-            type: 'success',
-            message: this.$t('accounts_view.new_success'),
-            buttonText: this.$t('buttons.close')
-          })
-        })
-        this.accountDialogStatusAction(false)
-        this.accountSchemaAction({
-          name: '',
-          description: '',
-          disabled: ''
-        })
-      },
-      // Delete account
-      deleteAccount (account) {
-        this.$apollo.mutate({
-          mutation: DELETE_ACCOUNT,
-          context: {
-            uri: 'account'
-          },
-          variables: {
-            ids: account._id
-          },
-          update: (store) => {
-            const data = store.readQuery({ query: GET_ACCOUNTS })
-            data.accounts = data.accounts.filter((item) => {
-              return item._id !== account._id
-            })
-            store.writeQuery({ query: GET_ACCOUNTS, data })
-          }
-        })
-      },
-      // Edit account
-      editAccount (id, name, description, disabled) {
-        this.$apollo.mutate({
-          mutation: UPDATE_ACCOUNT,
-          context: {
-            uri: 'account'
-          },
-          variables: {
-            id: id,
-            input: {
-              name: name,
-              description: description,
-              disabled: disabled
-            }
-          },
-          update: (store) => {
-            const data = store.readQuery({ query: GET_ACCOUNTS })
-            data.accounts.map((item) => {
-              if (item._id === id) {
-                item.description = description
-                item.disabled = disabled
-              }
-            })
-            store.writeQuery({ query: GET_ACCOUNTS, data })
-          }
-        }).then(() => {
-          this.editedIndexStatusAction(-1)
-          this.SET_ALERT_MESSAGE({
-            show: true,
-            type: 'success',
-            message: this.$t('accounts_view.edit_success'),
-            buttonText: this.$t('buttons.close')
-          })
-          this.accountDialogStatusAction(false)
-          this.accountSchemaAction({
-            name: '',
-            description: '',
-            disabled: ''
-          })
-        })
-      }
-    },
-    mounted () {
-      // Receive events from components
-      this.$root.$on('createAccount', (name, description, disabled) => {
-        this.createAccount(name, description, disabled)
-      })
-      this.$root.$on('deleteAccount', (account) => {
-        this.deleteAccount(account)
-      })
-      this.$root.$on('editAccount', (id, name, description, disabled) => {
-        this.editAccount(id, name, description, disabled)
-      })
-    },
-    beforeDestroy () {
-      this.$root.$off()
-    }
+    mixins: [accountMixin]
   }
 </script>
--- src/router/views/Dashboard-view.vue
+++ src/router/views/Dashboard-view.vue
@@ -12,7 +12,9 @@
 
 
 <script>
-  import { mapActions, mapGetters, mapMutations } from 'vuex'
+  import { mapActions, mapGetters } from 'vuex'
+  // Mixins
+  import accountMixin from '@/mixins/accountMixin'
   // Components imports
   import DashboardDataTable from '@/components/dashboard/DashboardDataTable'
   import DashboardTabs from '@/components/dashboard/DashboardTabs'
@@ -20,7 +22,6 @@
   import { APP_DATA } from '@/graphql/app'
   import { GET_DASHBOARD_REPORT_DATA } from '@/graphql/report'
   import { GET_DATA_FILTERS } from '@/graphql/config'
-  import { CREATE_NEW_ACCOUNT, GET_ACCOUNTS } from '@/graphql/account'
 
   export default {
     name: 'dashboard-view',
@@ -38,6 +39,7 @@
       DashboardDataTable,
       DashboardTabs
     },
+    mixins: [accountMixin],
     apollo: {
       stats: {
         query: GET_DASHBOARD_REPORT_DATA,
@@ -57,7 +59,6 @@
             }
           }
         },
-        // pollInterval: 100,
         loadingKey: 'loading',
         update (data) {
           this.statsDataAction(data.stats)
@@ -68,7 +69,6 @@
         context: {
           uri: 'config'
         },
-        // pollInterval: 100,
         loadingKey: 'loading',
         update (data) {
           this.dashboardFiltersAction(data.config)
@@ -87,65 +87,10 @@
     },
     methods: {
       ...mapActions([
-        'accountDialogStatusAction',
-        'accountSchemaAction',
         'appDataAction',
         'dashboardFiltersAction',
         'statsDataAction'
-      ]),
-      ...mapMutations(['SET_ALERT_MESSAGE']),
-      createAccount (name, description, disabled) {
-        this.$apollo.mutate({
-          mutation: CREATE_NEW_ACCOUNT,
-          context: {
-            uri: 'account'
-          },
-          variables: {
-            input: {
-              name: name,
-              description: description,
-              disabled: disabled || false
-            }
-          },
-          update: (store, { data: { createAccount } }) => {
-            // Read the data from our cache for this query.
-            const data = store.readQuery({ query: GET_ACCOUNTS })
-            // Add our tag from the mutation to the end
-            data.accounts.push(createAccount)
-            // Write our data back to the cache.
-            store.writeQuery({ query: GET_ACCOUNTS, data })
-          }
-        })
-        .then(() => {
-          this.SET_ALERT_MESSAGE({
-            show: true,
-            type: 'success',
-            message: this.$t('accounts_view.new_success'),
-            buttonText: this.$t('buttons.close')
-          })
-        })
-        this.accountDialogStatusAction(false)
-        this.accountSchemaAction({
-          name: '',
-          description: '',
-          disabled: ''
-        })
-      }
-    },
-    mounted () {
-      // Receive events from components
-      this.$root.$on('createAccount', (name, description, disabled) => {
-        this.createAccount(name, description, disabled)
-      })
-      this.$root.$on('deleteAccount', (account) => {
-        this.deleteAccount(account)
-      })
-      this.$root.$on('editAccount', (id, name, description, disabled) => {
-        this.editAccount(id, name, description, disabled)
-      })
-    },
-    beforeDestroy () {
-      this.$root.$off()
+      ])
     }
   }
 </script>
