209e28788064d18e537de617e42fbea03e485722
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-06-05 11:18:57 +0200

    Modificaciones app data table

--- src/components/apps/AppDataTable.vue
+++ src/components/apps/AppDataTable.vue
@@ -17,15 +17,27 @@
         :items="apps"
         :search="search"
         class="elevation-1 apps-datatable"
+        :rows-per-page-items="[10, 20, 30]"
         )
         template(slot="items" slot-scope="props")
-            td.text-xs-left <span>{{ props.item.platform }}</span> {{ props.item.name }}
-            td.text-xs-left {{ props.item.imps }}
-            td.text-xs-left {{ props.item.fillRate }}
-            td.text-xs-left {{ props.item.clicks }}
-            td.text-xs-left {{ props.item.ctr }}
-            td.text-xs-left {{ props.item.revenue }}
-            td.text-xs-left {{ props.item.ecpm }}
+            td(class="text-xs-left app-name") <span>{{ props.item.platform }}</span> {{ props.item.name }}
+            td.text-xs-left ADDCOLONY
+            td.text-xs-left ADMOB
+            td.text-xs-left APPLOVIN
+            td.text-xs-left CHARBOOST
+            td.text-xs-left CUSTOM CAMPAING
+            td.text-xs-left FACEBOOK
+            td.text-xs-left HYPRMX
+            td.text-xs-left KIIP
+            td.text-xs-left INMOBI
+            td.text-xs-left IRON SOURCE
+            td.text-xs-left MOBUSI
+            td.text-xs-left MOBUSI SSP
+            td.text-xs-left MOBVISTA
+            td.text-xs-left MOPUB
+            td.text-xs-left UNITYADS
+            td.text-xs-left STARTAPP
+            td.text-xs-left VUNGLE
 
         template(slot="no-data")
             v-alert(
@@ -46,19 +58,19 @@ export default {
         align: 'left',
         value: 'name'
       },
-      { text: 'Impressions', value: 'impressions' },
+      // { text: 'Impressions', value: 'impressions' },
       { text: 'ADCOLONY', value: 'ADCOLONY' },
       { text: 'ADMOB', value: 'ADMOB' },
       { text: 'APPLOVIN', value: 'APPLOVIN' },
       { text: 'CHARTBOOST', value: 'CHARTBOOST' },
-      { text: 'CUSTOM_CAMPAIGN', value: 'CUSTOM_CAMPAIGN' },
+      { text: 'CUSTOM CAMPAIGN', value: 'CUSTOM CAMPAIGN' },
       { text: 'FACEBOOK', value: 'FACEBOOK' },
       { text: 'HYPRMX', value: 'HYPRMX' },
       { text: 'KIIP', value: 'KIIP' },
       { text: 'INMOBI', value: 'INMOBI' },
       { text: 'IRONSOURCE', value: 'IRONSOURCE' },
       { text: 'MOBUSI', value: 'MOBUSI' },
-      { text: 'MOBUSI_SSP', value: 'MOBUSI_SSP' },
+      { text: 'MOBUSI SSP', value: 'MOBUSI SSP' },
       { text: 'MOBVISTA', value: 'MOBVISTA' },
       { text: 'MOPUB', value: 'MOPUB' },
       { text: 'UNITYADS', value: 'UNITYADS' },
@@ -78,6 +90,9 @@ export default {
 <style lang="scss" scoped>
 td {
   cursor: pointer;
+}
+.app-name {
+  min-width: 250px;
 }
 </style>
 
