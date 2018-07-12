6367a540d973104dc708f15872ca7ec40d59ab8f
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-07-12 09:51:34 +0200

    config API

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,5 +7,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_BASE_URL: "'http://stage.do.linkitox.com/public/graphql'"
+  API_BASE_URL: "'http://qa.stage.do.linkitox.com/public/graphql'"
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: "'http://stage.do.linkitox.com/public/graphql'"
+  API_BASE_URL: "'http://qa.stage.do.linkitox.com/public/graphql'"
 }
