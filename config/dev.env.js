7757e338d712b4f6e31289efbf3c5bf6c98c1f67
Alejandro Cuiñas <alejandro_web@hotmail.com>
2018-07-12 09:43:05 +0200

    configuración API URL

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,10 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
+// qa.stage.do.linkitox.com
+// stage.do.linkitox.com
+
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_BASE_URL: "'http://stage.do.linkitox.com/public/graphql'"
 })
